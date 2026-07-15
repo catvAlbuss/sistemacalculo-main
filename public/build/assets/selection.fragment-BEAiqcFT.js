82f506f2bd9a9c71b809e95f31b273d31a52e3d4
catvAlbuss <programdesing.rizabalasociados@gmail.com>
2026-07-15 11:42:09 -0500

    Mejoramientos de Server

--- public/build/assets/selection.fragment-BEAiqcFT.js
+++ public/build/assets/selection.fragment-BEAiqcFT.js
@@ -1,4 +1,4 @@
-import{S as i}from"./analisis_estructural_de_armaduras-DkDKdulO.js";import"./livewire.esm-CO6ZehnT.js";import"./predimensionamiento-CxryAhEz.js";import"./preload-helper-BfFHrpNk.js";import"./mat4js.read-DcrXhSIi.js";import"./_commonjsHelpers-_d1bhYXs.js";import"./___vite-browser-external_commonjs-proxy-BQ48sp8I.js";const e="selectionPixelShader",r=`#ifdef INSTANCES
+import{S as i}from"./analisis_estructural_de_armaduras-KyqQRvW4.js";import"./livewire.esm-CO6ZehnT.js";import"./predimensionamiento-CxryAhEz.js";import"./preload-helper-BfFHrpNk.js";import"./mat4js.read-DcrXhSIi.js";import"./_commonjsHelpers-_d1bhYXs.js";import"./___vite-browser-external_commonjs-proxy-BQ48sp8I.js";const e="selectionPixelShader",r=`#ifdef INSTANCES
 flat varying vSelectionId: f32;
 #else
 uniform selectionId: f32;
